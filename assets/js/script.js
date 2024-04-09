4213f3ebc72b8adcfb639f8b4aae95a3625168c2
Brian Lee <brian-lee@live.co.uk>
2024-04-09 13:38:04 +0100

    Reformatting of comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,23 @@
-// IMAGE SLIDES & CIRCLES ARRAYS, & COUNTER
+// Image slide, Circle array, & counter
 var imageSlides = document.getElementsByClassName('imageSlides');
 var circles = document.getElementsByClassName('circle');
 var counter = 0;
 
-// HIDE ALL IMAGES FUNCTION
+// Hide images function
 function hideImages() {
   for (var i = 0; i < imageSlides.length; i++) {
     imageSlides[i].classList.remove('visible');
   }
 }
 
-// REMOVE ALL DOTS FUNCTION
+// Remove dots function
 function removeDots() {
   for (var i = 0; i < imageSlides.length; i++) {
     circles[i].classList.remove('dot');
   }
 }
 
-// IMAGE LOOP/CIRCLES FUNCTION
+// Image loop function
 function imageLoop() {
   var currentImage = imageSlides[counter];
   var currentDot = circles[counter];
@@ -27,7 +27,7 @@ function imageLoop() {
   counter++;
 }
 
-// IMAGE SLIDE FUNCTION
+// Image slide function
 function slideshow() {
   if (counter < imageSlides.length) {
     imageLoop();
@@ -38,6 +38,6 @@ function slideshow() {
   }
 }
 
-// SHOW FIRST IMAGE, & THEN SET & CALL SLIDE INTERVAL
+// Shows first image and slide interval
 setTimeout(slideshow, 1000);
 var imageSlideshowInterval = setInterval(slideshow, 5500);
