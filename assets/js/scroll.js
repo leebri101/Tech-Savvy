64f1b2e8afc1dc60fc1512fa98b3f7cb3bc020be
Brian Lee <brian-lee@live.co.uk>
2024-04-15 12:57:17 +0100

    Scroll animtion script complete

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -4,3 +4,45 @@ window.addEventListener('scroll', function() {
 var scroll = window.requestAnimationFrame ||
     function(callback) { window.setTimeout(callback, 1000/60) };
 var elementsToShow = document.querySelectorAll('.show-on-scroll');
+
+function loop() {
+    elementsToShow.forEach(function(element){
+        if (isElementInViewport(element)) {
+            element.classList.add('is-visible');
+        } else {
+            element.classList.remove('is-visible');
+        }
+    });
+    scroll(loop);
+} 
+loop();
+
+function isElementInViewport(el) {
+    if (typeof jQuery === "function" && el instanceof jQuery) {
+        el = el[0];
+    }
+    var rect = el.getBoundingClientRect();
+    return (
+        (rect.top <= 0
+            && rect.bottom >= 0)
+        ||
+        (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+        ||
+        (rect.top >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+    );
+}
+
+const callback = function(entries) {
+    entries.forEach(entry => {
+        entry.target.classList.toggle("is-visible");
+    });
+}
+
+const observer = new IntersectionObserver(callback);
+
+const targets = document.querySelectorAll(".show-on-scroll");
+targets.forEach(function(target) {
+    observer.observe(target);
+});
