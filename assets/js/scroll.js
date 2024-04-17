1c1180d9636e540d7ba6c61949a45da7a9fe99bb
Brian Lee <brian-lee@live.co.uk>
2024-04-17 10:37:32 +0100

    Restructure of scroll script code

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,50 +1,39 @@
 // Window scroll event listener
-window.addEventListener('scroll', function() {
-    console.log('scrolling');
-});
 var scroll = window.requestAnimationFrame ||
-    function(callback) { window.setTimeout(callback, 1000/60) };
-var elementsToShow = document.querySelectorAll('.show-on-scroll');
+             //Fallback
+             function(callback){ window.setTimeout(callback, 1000/60)};
+var elementsToShow = document.querySelectorAll('.show-on-scroll'); 
 
 function loop() {
-    elementsToShow.forEach(function(element){
-        if (isElementInViewport(element)) {
-            element.classList.add('is-visible');
-        } else {
-            element.classList.remove('is-visible');
-        }
+
+    Array.prototype.forEach.call(elementsToShow, function(element){
+      if (isElementInViewport(element)) {
+        element.classList.add('is-visible');
+      } else {
+        element.classList.remove('is-visible');
+      }
     });
+
     scroll(loop);
-} 
+}
+
 loop();
 
-// jQuery fuctionality
+// Helper function from stackoverflow
 function isElementInViewport(el) {
+    // jQuery functionality
     if (typeof jQuery === "function" && el instanceof jQuery) {
         el = el[0];
-    }
-    var rect = el.getBoundingClientRect();
-    return (
+      }
+      var rect = el.getBoundingClientRect();
+      return (
         (rect.top <= 0
-            && rect.bottom >= 0)
+          && rect.bottom >= 0)
         ||
         (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
-            rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+          rect.top <= (window.innerHeight || document.documentElement.clientHeight))
         ||
         (rect.top >= 0 &&
-            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
-    );
-}
-
-const callback = function(entries) {
-    entries.forEach(entry => {
-        entry.target.classList.toggle("is-visible");
-    });
-}
-
-const observer = new IntersectionObserver(callback);
-
-const targets = document.querySelectorAll(".show-on-scroll");
-targets.forEach(function(target) {
-    observer.observe(target);
-});
+          rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+      );
+    }
