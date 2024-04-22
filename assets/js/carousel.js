7f16415b795cb7606b8c572beab0f6fd5d28ebe2
Brian Lee <brian-lee@live.co.uk>
2024-04-22 14:13:23 +0100

    Change of animation and styling for carousel

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -3,30 +3,37 @@ const carouselInner = carousel.querySelector('.carousel-inner');
 const carouselItems = carouselInner.children;
 const prevArrow = carousel.querySelector('.prev-arrow');
 const nextArrow = carousel.querySelector('.next-arrow');
-
 let currentIndex = 0;
 
 function moveToSlide(index) {
-  if (index < 0) index = carouselItems.length - 1;
-  if (index > carouselItems.length - 1) index = 0;
-
   for (let i = 0; i < carouselItems.length; i++) {
     carouselItems[i].classList.remove('active');
   }
-
   carouselItems[index].classList.add('active');
   currentIndex = index;
 }
 
 function moveToNextSlide() {
-  moveToSlide(currentIndex + 1);
+  if (currentIndex >= carouselItems.length - 1) {
+    moveToSlide(0);
+  } else {
+    moveToSlide(currentIndex + 1);
+  }
 }
 
 function moveToPrevSlide() {
-  moveToSlide(currentIndex - 1);
+  if (currentIndex === 0) {
+    moveToSlide(carouselItems.length - 1);
+  } else {
+    moveToSlide(currentIndex - 1);
+  }
 }
 
 prevArrow.addEventListener('click', moveToPrevSlide);
 nextArrow.addEventListener('click', moveToNextSlide);
 
 moveToSlide(currentIndex);
+
+setInterval(() => {
+  moveToNextSlide();
+}, 5000);
